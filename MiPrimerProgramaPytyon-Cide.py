d19b7d254d41af65cc79768894a3a5a1d4f92faa
Nicolas-Cide <93502044+Nicolas-Cide@users.noreply.github.com>
2022-06-13 00:37:13 -0300

    Update MiPrimerProgramaPytyon-Cide.py

--- MiPrimerProgramaPytyon-Cide.py
+++ MiPrimerProgramaPytyon-Cide.py
@@ -2,11 +2,12 @@ print("---- Se da inicio al programa que calcula la nota final del alumno en bas
 
 Nota1 = int(input("Ingresar Nota 1: "))
 Nota2 = int(input("Ingresar Nota 2: "))
-PorcNota1 = 40
-PorcNota2 = 60
+PorcNota1 = int(input("Ingrese porcentaje de Nota 1: "))
+PorcNota2 = int(input("Ingrese porcentaje de Nota 2: "))
 
-ResNota1 = (PorcNota1*Nota1)/100
-ResNota2 = (PorcNota2*Nota2)/100
-NotaFinal = ResNota1 + ResNota2
+PorcNota1 = PorcNota1/100
+PorcNota2 = PorcNota2/100
+
+NotaFinal = ((PorcNota1*Nota1)+(PorcNota2*Nota2))
 
 print("La nota final del alumno es: " + str(float(NotaFinal)))
